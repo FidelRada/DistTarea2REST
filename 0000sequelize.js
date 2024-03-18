c31fdba47ce6c0f133435644ec7c2325b68e572f
FidelRada <50565740+FidelRada@users.noreply.github.com>
2024-03-18 13:08:46 -0400

    Delete 0000sequelize.js
    no sirve

--- 0000sequelize.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('tarea1', 'postgres', '60910990', {
-  host: 'localhost',
-  dialect: 'postgres', // Indica que estás utilizando PostgreSQL
-  port: 5432, // Puerto por defecto de PostgreSQL
-});
-
-module.exports = sequelize;
